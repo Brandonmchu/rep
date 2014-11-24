c07c2962202af63fce6917fb7d978c7d37f2719a
Brandonmchu <brandon.mk.chu@gmail.com>
2014-11-23 21:41:33 -0500

    changes starting datetime

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,12 +7,12 @@ class SalesController < ApplicationController
     longitude = params[:longitude] || -79.38318429999998
     proximity = params[:proximity] || 1000
     if params[:start_date].nil?
-      @start_date = Time.now - 50.days
+      @start_date = DateTime.civil_from_format :local, 2014, 8, 1
     else
       @start_date = Date.parse(params[:start_date])
     end
     if params[:end_date].nil?
-      @end_date = Time.now
+      @end_date = DateTime.civil_from_format :local, 2014, 9, 1
     else
       @end_date = Date.parse(params[:end_date])
     end 
